e922d050fae8fe34281fac6fdd2908c1d72a389b
Deven <DevenThiel@gmail.com>
2016-02-05 19:31:23 -0500

    Updated lab checking

--- src/Labs/lab1_4Checks.java
+++ src/Labs/lab1_4Checks.java
@@ -20,7 +20,84 @@ public class lab1_4Checks extends inputChecks
                 {
                     setKey(i,j,"*");
                     
-                    //TODO
+                    if ((i==1 || i==2 || i==18 || i==19) && (j == 0 || j==1))
+                    {
+                        setKey(i,j,setToDecPlaces(getData(i,j),2));
+                    }
+                    
+                    if ((i > 2 && i < 18) && (j == 0 || j==1 || j==2))
+                    {
+                        setKey(i,j,setToDecPlaces(getData(i,j),1));
+                    }
+                            
+                     if ((i > 19 && i < 23) && (j == 0 || j==1))
+                    {
+                        setKey(i,j,setToDecPlaces(getData(i,j),1));
+                    }
+                     
+                     if ( i==18 && (j == 0 || j==1))
+                    {
+                        setKey(i,j,getData(1,j));
+                    }
+                     
+                     if ( i==19 && (j == 0 || j==1))
+                    {
+                        setKey(i,j,getData(2,j));
+                    }
+                     
+                     if ( i==22 && (j == 0 || j==1))
+                    {
+                        double temp = Double.parseDouble(getData(20,j)) -
+                                Double.parseDouble(getData(21,j));
+                                              
+                        setKey(i, j,"" + temp); 
+                    }
+                     
+                     if ( i==23 && (j == 0 || j==1))
+                    {
+                        setKey(i, j,"20.8"); 
+                    }
+                    
+                     if ( i==24 && (j == 0 || j==1))
+                    {
+                        double temp = Double.parseDouble(getData(22,j)) /
+                                Double.parseDouble(getData(23,j));
+                                              
+                        setKey(i, j,"" + temp); 
+                    }
+                     
+                     if ( i==25 && (j == 0 || j==1))
+                    {
+                        double temp = Double.parseDouble(getData(24,j)) *
+                                Double.parseDouble(getData(18,j)) / 1000;
+                                              
+                        setKey(i, j,"" + temp); 
+                    }
+                    
+                     if ( i==26 && (j == 0 || j==1))
+                    {
+                        double temp = Double.parseDouble(getData(19,j)) /
+                                Double.parseDouble(getData(25,j));
+                                              
+                        setKey(i, j,"" + temp); 
+                    }
+                     
+                      if (i==27 && j == 0 )
+                    {
+                        double temp = (Double.parseDouble(getData(26,0)) +
+                                Double.parseDouble(getData(26,1)) +
+                                Double.parseDouble(getData(26,2))) / 3;
+                                              
+                        setKey(i, j,"" + temp); 
+                    }
+                      
+                      if (((i > 23 && i < 27) && (j == 0 || j==1)) || i==27 && j==0)
+                    {
+                        if (getSigFigs(getData(i,j)) != 2)
+                        {
+                            setKey(i,j,"WRONG");
+                        }
+                    }
                     
                 }
                 else
