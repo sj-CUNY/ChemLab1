8863b4bf6579f4c341bcf401ba6da928db80f2ab
mcardenas389 <mcardenas389@users.noreply.github.com>
2016-07-07 10:48:53 -0400

    Add files via upload

--- src/Labs/lab0_1Checks.java
+++ src/Labs/lab0_1Checks.java
@@ -6,35 +6,54 @@ import blackboard.platform.context.Context;
 
 public class lab0_1Checks extends inputChecks
 {
-	public lab0_1Checks(Context ctx, int x, int y, String labname, String userid, String courseid)
+	public lab0_1Checks(Context ctx, int x, int y, int userid, String courseid, int labNumber)
 	{
-        super(ctx, x,y, labname, userid, courseid);
+        super(ctx, x, y, userid, courseid, labNumber);
     }
  
 	@Override
     protected void buildKey()
     {
-        for (int i = 0; i < dataX; i ++)
+		int sigFig = 0;
+		
+        for (int i = 0; i < dataX; ++i)
         {
-            for (int j = 0; j < dataY; j++)
+            for (int j = 0; j < dataY; ++j)
             {
-                if (data[i][j] != null && !data[i][j].equals(""))
+                if(data[i][j] != null && !data[i][j].equals(""))
                 {
-                    setKey(i,j,"*");
+                    setKey(i,j,"*"); //where there is no data to input
                     
-                    //Quadruple beam balance
-                    if ((i > 1 && i < 4) && j == 0)
+                    //Quadruple beam balance (sig figs)
+                    if (i > 1 && i < 4)
                     {
-                        //setKey(i,j,setToDecPlaces(getData(i,j),3));
-                    	setKey(i,j,"3");
+                    	if(j == 0)
+                    	{
+                    		//setKey(i,j,setToDecPlaces(getData(i,j),3));
+	                    	setKey(i,j,"3");
+                    	}
+                    	else if(j == 2)
+                    	{
+                    		sigFig = getSigFigs(data[i][j]) + 3;
+                    		setKey(i, j, Integer.toString(sigFig));
+                    	}
                     }
-                    //Analytical balance
-                    if ((i > 3 && i < 10) && j == 0)
+                    
+                    //Analytical balance (sig figs)
+                    if(i > 3 && i < 10)
                     {
                         //setKey(i,j,setToDecPlaces(getData(i,j),4));
-                        setKey(i,j,"4");
+                    	if(j == 0) 
+                    	{
+                    		setKey(i,j,"4");
+                    	}
+                    	else if(j == 2)
+                    	{
+                    		sigFig = getSigFigs(data[i][j]) + 4;
+                    		setKey(i, j, Integer.toString(sigFig));
+                    	}
                     }
-                  //Results
+                    //Results
                     /*
                     if ((i > 9 && i < 12) && j == 0)
                     {
@@ -42,11 +61,13 @@ public class lab0_1Checks extends inputChecks
                         setKey(i,j,"4");
                     }
                     */
+                    
                     //Unit column
                     if ((i > 1 && i < 12) && j == 1)
                     {
                         setKey(i,j,"g");
                     }
+                    
                     //Num of Sig Fig Column
                     if ((i > 1 && i < 12) && j == 2)
                     {
@@ -103,4 +124,82 @@ public class lab0_1Checks extends inputChecks
             }
         }
     }
+	
+	protected void labCheck()
+	{
+		//check significant figures of quadruple and analytical balance 
+		for(int i = 2; i < 10; ++i)
+		{
+			if(getSigFigs(data[i][0]) + Integer.parseInt(key[i][0]) == Integer.parseInt(key[i][2]))
+			{
+				isCorrect[i][0] = correctMsg; //weighed value
+				isCorrect[i][2] = correctMsg; //significant figures
+			}
+			else
+			{
+				isCorrect[i][0] = errorMsg;
+				isCorrect[i][2] = errorMsg;
+				error[i][0] = errorTypes[3]; //generic error
+				error[i][2] = errorTypes[0]; //sig fig error
+			}
+		}
+		
+		//check calculation of average metal weight
+		if(data[10][0] == key[10][0])
+		{
+			isCorrect[10][0] = correctMsg;
+		}
+		else
+		{
+			isCorrect[10][0] = errorMsg;
+			error[10][0] = errorTypes[1]; //calculation error
+		}
+		
+		//check significant figures
+		if(data[10][2] == key[10][2])
+		{
+			isCorrect[10][2] = correctMsg;
+		}
+		else
+		{
+			isCorrect[10][2] = errorMsg;
+			error[10][2] = errorTypes[0]; //sig fig error
+		}
+		
+		//check calculation of sodium chloride weight
+		if(data[11][0] == key[11][0])
+		{
+			isCorrect[11][0] = correctMsg;
+		}
+		else
+		{
+			isCorrect[11][0] = errorMsg;
+			error[11][0] = errorTypes[1]; //calculation error
+		}
+		
+		//check significant figures
+		if(data[11][2] == key[11][2])
+		{
+			isCorrect[11][2] = correctMsg;
+		}
+		else
+		{
+			isCorrect[11][2] = errorMsg;
+			error[11][2] = errorTypes[0]; //sig fig error
+		}
+		
+		//check units
+		for(int i = 2; i < dataX; ++i)
+		{
+			if(data[i][1] == key[i][1])
+			{
+				isCorrect[i][1] = correctMsg;
+			}
+			else
+			{
+				isCorrect[i][1] = errorMsg;
+				error[i][1] = errorTypes[2]; //unit error
+			}
+		}		
+	}
 }
